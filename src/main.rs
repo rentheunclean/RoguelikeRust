2f180d3735fa449c14566db700e93f504f720f51
Ren the Unclean <rentheunclean@gmail.com>
2021-06-29 15:16:48 -0700

    Initial rltk and specs setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let gs = State{ };
+    rltk::main_loop(context, gs)
 }
