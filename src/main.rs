fa745c5887c3b1d34be126a6f2da0435babaa370
Ren the Unclean <rentheunclean@gmail.com>
2021-06-30 16:12:29 -0700

    Creating and drawing map tiles

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,53 @@ struct Renderable
 #[derive(Component)]
 struct Player {}
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType
+{
+    Wall, Floor
+}
+
+
 struct State 
 {
     ecs: World,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize 
+{
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 
+    {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50
+    {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // randomly place walls
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400
+    {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25)
+        {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
 }
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) 
@@ -55,6 +99,33 @@ fn player_input(gs: &mut State, ctx: &mut Rltk)
     }
 }
 
+fn draw_map(map: &[TileType], ctx : &mut Rltk) 
+{
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter()
+    {
+        // Render a tile depending on the tile TileType
+        match tile 
+        {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 
+        {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 impl GameState for State 
 {
     fn tick(&mut self, ctx : &mut Rltk) 
@@ -63,6 +134,9 @@ impl GameState for State
 
         player_input(self, ctx);
         self.run_systems();
+        
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -95,6 +169,9 @@ fn main() -> rltk::BError
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+
+    // map
+    gs.ecs.insert(new_map());
 
     // Player
     gs.ecs
